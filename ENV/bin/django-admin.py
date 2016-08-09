4fe51b9d8eac74623f7c79898fe29456f2650d76
Junnian <junniancao@hotmail.com>
2016-08-09 22:07:01 +0800

    my simple blog first commit

---
+++ ENV/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/junnian/833569d8-8375-4408-a478-0ce91b06d092/junnian/django工程/simpleblog/ENV/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
