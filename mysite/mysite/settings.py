ad5ba223e4d4fbbb59e7ae6b659b109ac7616010
Junnian <junniancao@hotmail.com>
2016-08-10 21:02:32 +0800

    setting中zh-cn改成了zh-hans

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'zh-cn'
+LANGUAGE_CODE = 'zh-Hans'
 
 TIME_ZONE = 'Asia/Shanghai'
 
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
+
